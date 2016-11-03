cb2989d61bf1859598aa267d0e1c2b71e12bacfe
Pradita Utama <praditau@xl.co.id>
2016-11-03 09:51:21 +0700

    add Google Maps Javascript

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -8,8 +10,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+ 
   constructor(public navCtrl: NavController) {
-    
+ 
+  }
+ 
+  ionViewDidEnter(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(-6.226267, 106.825374);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    google.maps.event.trigger(this.map, 'resize');
+ 
   }
 
 }
